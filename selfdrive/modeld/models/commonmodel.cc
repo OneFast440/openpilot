937f8cc2f9b0fda8909a48c883dcef84d5faa7c4
Bruce Wayne <harald.the.engineer@gmail.com>
2024-09-29 23:47:29 -0700

    fix

--- selfdrive/modeld/models/commonmodel.cc
+++ selfdrive/modeld/models/commonmodel.cc
@@ -27,7 +27,7 @@ uint8_t* ModelFrame::prepare(cl_mem yuv_cl, int frame_width, int frame_height, i
                 yuv_cl, frame_width, frame_height, frame_stride, frame_uv_offset,
                 y_cl, u_cl, v_cl, MODEL_WIDTH, MODEL_HEIGHT, projection);
 
-  copy_queue(&loadyuv, q, img_buffer_20hz_cl, img_buffer_20hz_cl, 0, frame_size_bytes, frame_size_bytes);
+  copy_queue(&loadyuv, q, img_buffer_20hz_cl, img_buffer_20hz_cl, 0, frame_size_bytes, 4*frame_size_bytes);
   loadyuv_queue(&loadyuv, q, y_cl, u_cl, v_cl, last_img_cl);
   if (output == NULL) {
     CL_CHECK(clEnqueueReadBuffer(q, img_buffer_20hz_cl, CL_TRUE, 0, frame_size_bytes, &input_frames[0], 0, nullptr, nullptr));
