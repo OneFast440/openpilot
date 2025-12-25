4595dc7cd8ccafa2a47f8081b3100d9ab853c441
OneFast440 <onefast440@hotmail.com>
2025-12-25 13:27:46 -0700

    Add SunnyLink adjustable parameters to params_keys
    Added new adjustable parameters for SunnyLink.

--- common/params_keys.h
+++ common/params_keys.h
@@ -265,4 +265,11 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
     {"TorqueParamsOverrideEnabled", {PERSISTENT | BACKUP, BOOL, "0"}},
     {"TorqueParamsOverrideFriction", {PERSISTENT | BACKUP, FLOAT, "0.1"}},
     {"TorqueParamsOverrideLatAccelFactor", {PERSISTENT | BACKUP, FLOAT, "2.5"}},
+
+    // NEW: SunnyLink "Other" adjustable params
+    {"HumanTurnReset", {PERSISTENT | BACKUP, BOOL, "0"}},                       // On/Off
+    {"PredictedCurvatureBlendRatio", {PERSISTENT | BACKUP, FLOAT, "0.2"}},      // 0.20 - 0.60 step 0.1
+    {"LaneChangeFactor", {PERSISTENT | BACKUP, FLOAT, "0.5"}},                  // 0.5 - 1.0 step 0.05
+    {"EnableAdvancedLanePositioning", {PERSISTENT | BACKUP, BOOL, "0"}},        // On/Off
+    {"EnableLanefullMode", {PERSISTENT | BACKUP, BOOL, "0"}},  
 };
